134b5fe5e8be46da8c1c5b8acd320d38b93c0547
JohnnyCulbreth <johnnyculbrethjr@yahoo.com>
2022-12-30 22:42:06 -0600

    removed test code

--- index.js
+++ index.js
@@ -6,19 +6,3 @@ const AlphaVantage = algotrader.Data.AlphaVantage;
 const av = new AlphaVantage(`${process.env.AV_KEY}`);
 
 const app = express();
-
-// Get real time intraday price information
-av.timeSeriesIntraday("AAPL", "1min").then(array => {
-    // Returns an array of Quote objects for every minute since market open
-    array.forEach(quote => {
-        console.log( quote.getOpen() );   // 174.78
-        console.log( quote.getVolume() ); // 13523
-    });
-});
-// Get relative strength index
-av.rsi("AAPL", "daily", 14, "close").then(array => {
-    // Returns an array of objects representing the RSI on each day
-    array.forEach(rsi => {
-        // { date: 2017-11-17T00:00:00.000Z, RSI: '57.3707' }
-    });
-});
